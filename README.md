a7df5ef7783e70515c2c6e6644662336954adddc
Bryan Montecino <lizamamobj@vcu.edu>
2023-10-06 15:25:40 -0400

    deleting junk

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ The goal of this assignment is to create a C program which:
 * takes a text file as input
 * tokenizes the given input
 * Parses the tokenized input to determine if it is grammatically valid 
+
+How to run:
+gcc -std=c99 -o tokenizer main.c Tokenizer.c Givens.c
+./tokenizer test1.txt
--- Tokenizer.c
+++ Tokenizer.c
@@ -1,9 +1,7 @@
 #include "Tokenizer.h"
 #include "Givens.h"
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
-
 
 // Helper function to add a token to the lexics array
 static void addToken(struct lexics *aLex, int *numLex, enum token token, const char *lexeme) {
@@ -23,9 +21,8 @@ _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf) {
     const char* characterLexemes = "();{}";
 
     while ((currentChar = fgetc(inf)) != EOF) {
-        // Check if the current character is a character lexeme
         if (strchr(characterLexemes, currentChar) != NULL) {
-            // Check if we were in the middle of a lexeme
+            // If it's a character lexeme, treat it as a separator
             if (inLexeme) {
                 lexeme[lexemeIndex] = '\0'; // Null-terminate the lexeme
                 inLexeme = FALSE;
@@ -41,23 +38,24 @@ _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf) {
             // Handle the character lexeme
             char charLexeme[2] = {currentChar, '\0'};
             addToken(aLex, &tokenIndex, BINOP, charLexeme);
-        } else if (!isspace(currentChar)) {
+        } else if (currentChar == ' ' || currentChar == '\t' || currentChar == '\n') {
+            // If it's a space, tab, or newline, treat it as a separator
+            if (inLexeme) {
+                lexeme[lexemeIndex] = '\0'; // Null-terminate the lexeme
+                inLexeme = FALSE;
+                // Tokenize the lexeme and add it to aLex
+                if (validNumber(lexeme)) {
+                    addToken(aLex, &tokenIndex, NUMBER, lexeme);
+                } else if (validIdentifier(lexeme)) {
+                    addToken(aLex, &tokenIndex, IDENTIFIER, lexeme);
+                } else {
+                    // Handle other cases as needed
+                }
+            }
+        } else {
             // Handle non-space characters
-            // Add the character to the lexeme
             lexeme[lexemeIndex++] = currentChar;
             inLexeme = TRUE;
-        } else if (inLexeme) {
-            // We were in the middle of a lexeme, but we encountered whitespace
-            lexeme[lexemeIndex] = '\0'; // Null-terminate the lexeme
-            inLexeme = FALSE;
-            // Tokenize the lexeme and add it to aLex
-            if (validNumber(lexeme)) {
-                addToken(aLex, &tokenIndex, NUMBER, lexeme);
-            } else if (validIdentifier(lexeme)) {
-                addToken(aLex, &tokenIndex, IDENTIFIER, lexeme);
-            } else {
-                // Handle other cases as needed
-            }
         }
     }
 
